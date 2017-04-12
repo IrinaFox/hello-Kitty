208578220572bf0353d76bb77c37e4de60b42d7e
IrinaFox <irina.fox88@gmail.com>
2017-04-13 01:47:11 +0300

    categories buttons are seen immediately

--- src/app/categories/categories.controller.js
+++ src/app/categories/categories.controller.js
@@ -1,17 +1,40 @@
 export class CategoriesController {
   constructor ($log, $http) {
     'ngInject';
-    var _categories = [];
+
+    //var _categories;
+
+    this.categories = [
+      {
+        "name": "Web UI",
+        "id": "0"
+      },
+
+      {
+        "name": "Python",
+        "id": "1"
+      },
+
+      {
+        "name": "Database",
+        "id": "2"
+      },
+
+      {
+        "name": "Manual Quality",
+        "id": "3"
+      }
+    ];
 
     this.getCategories = function () {
       $log.log('Ediiiit');
 
       $http.get('/categories')
         .then((response) => {
-          _categories = response.data;
-          $log.log(_categories);
+          //_categories = response.data;
+          $log.log(response.data);
         });
-      this.categories = _categories;
+      //this.categories = _categories;
     };
 
     this.sayHi = function () {
