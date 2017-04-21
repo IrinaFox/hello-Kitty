bbcf5553e50f538887d487d70a0d05a55bb8fe8e
Svetlana Ovcharenko <dark1bloom@gmail.com>
2017-04-21 17:13:46 +0300

    after demo

--- src/app/feedbacks/feedbacks.modalcontrollerfeedbacks.js
+++ src/app/feedbacks/feedbacks.modalcontrollerfeedbacks.js
@@ -2,14 +2,11 @@ export class ModalControllerFeedbacks {
     constructor ($uibModalInstance, $scope, newFeedbacks) {
         'ngInject';
         this.uibModalInstance = $uibModalInstance;
-        this.newFeedbacks = newFeedbacks;
+        this.feedback = newFeedbacks;
     }
 
     save (feedback) {
-        this.newFeedbacks.raiting = feedback.raiting || this.newFeedbacks.raiting;
-        this.newFeedbacks.comment = feedback.comment || this.newFeedbacks.comment;
-        this.newFeedbacks.author = feedback.author || this.newFeedbacks.author;
-        this.uibModalInstance.close(this.newFeedbacks);
+        this.uibModalInstance.close(this.feedback);
     }
 
     cancel () {
