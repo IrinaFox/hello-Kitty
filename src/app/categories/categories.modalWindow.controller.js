e44adf4e0d8ea516749f857eafdedbdc74134d65
IrinaFox <irina.fox88@gmail.com>
2017-04-23 23:50:56 +0300

    categories

--- src/app/categories/categories.modalWindow.controller.js
+++ src/app/categories/categories.modalWindow.controller.js
@@ -1,4 +1,4 @@
-export class CategoriesModalWindow {
+export class CategoriesModalWindowController {
     constructor ($uibModalInstance, $scope, $http, categoriesList) {
         'ngInject';
         this.uibModalInstance = $uibModalInstance;
--- src/app/categories/categories.module.js
+++ src/app/categories/categories.module.js
@@ -1,12 +1,12 @@
 import { categoriesRouter } from './categories.router';
 import { CategoriesController } from './categories.controller';
 import { GithubContributorService } from '../components/githubContributor/githubContributor.service';
-import { CategoriesModalWindow } from './categories.modalEdition.controller';
+import { CategoriesModalWindowController } from './categories.modalEdition.controller';
 
 var categories = angular.module('categories', ['ui.router', 'ui.bootstrap'])
     .config(categoriesRouter)
     .service('githubContributor', GithubContributorService)
-    .controller('CategoriesModalWindow', CategoriesModalWindow)
+    .controller('CategoriesModalWindowController', CategoriesModalWindowController)
     .controller('CategoriesController', CategoriesController);
 
 export default categories;
