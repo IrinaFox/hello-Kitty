5d55bb8102044a1c830eb17e67b3ad0196faba4e
IrinaFox <irina.fox88@gmail.com>
2017-04-25 18:10:51 +0300

    clear test

--- src/app/categories/categories.controller.spec.js
+++ src/app/categories/categories.controller.spec.js
@@ -12,20 +12,4 @@ describe('CategoriesController', () => {
         $http.get('/categories');
         expect(vm.categoriesList).toEqual(undefined);
     }));
-
-    /*it('should define animate class after delaying timeout', inject($timeout => {
-      $timeout.flush();
-      expect(vm.classAnimation).toEqual('rubberBand');
-    }));
-
-    it('should show a Toastr info and stop animation when invoke showToastr()', inject(toastr => {
-      vm.showToastr();
-      expect(toastr.info).toHaveBeenCalled();
-      expect(vm.classAnimation).toEqual('');
-    }));
-
-    it('should define more than 5 awesome things', () => {
-      expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
-      expect(vm.awesomeThings.length === 5).toBeTruthy();
-    });*/
 });
